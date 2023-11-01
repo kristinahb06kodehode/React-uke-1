f70c2cdc7dd80479e03f13547b5182e41c4c48bb
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2023-11-01 13:30:48 +0100

    css and html tsx

--- react-webpahe/src/App.tsx
+++ react-webpahe/src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="app">
+      <header className="navbar">
+        <div className="logoCircle"></div>
+        <nav>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h1>Blog Deluxe</h1>
+        <form action="">
+          <input type="search" placeholder="Search..." />
+        </form>
+      </main>
+
+      <section>
+        <div className="userInf">
+          <div className="circle"></div>
+          <h4>Anah Benesova</h4>
+          <button></button>
+        </div>
+        <p className="date">December 9, 2023</p>
+        <h3>
+          If we quantify the alarm, we can get to the <br></br> FTP pixel
+          through the online SSL interface!{" "}
+        </h3>
+        <p className="text-content">
+          Omnis perspiciatis qui quia commodi sequi modi. Nostrum quam aut
+          cupiditate est facere omnis possimus. Tenetur similique nemo illo
+          soluta molestias facere quo. Ipsam totam facilis delectus nihil quidem
+          soluta vel est omnis.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <p className="readMore">Read more...</p>
+      </section>
+      <section>
+        <div className="userInf">
+          <div className="circle"></div>
+          <h4>Anah Benesova</h4>
+          <button></button>
+        </div>
+        <p className="date">December 9, 2023</p>
+        <h3>
+          If we quantify the alarm, we can get to the <br></br> FTP pixel
+          through the online SSL interface!{" "}
+        </h3>
+        <p className="text-content">
+          Omnis perspiciatis qui quia commodi sequi modi. Nostrum quam aut
+          cupiditate est facere omnis possimus. Tenetur similique nemo illo
+          soluta molestias facere quo. Ipsam totam facilis delectus nihil quidem
+          soluta vel est omnis.
+        </p>
+        <p className="readMore">Read more...</p>
+      </section>
+      <section>
+        <div className="userInf">
+          <div className="circle"></div>
+          <h4>Anah Benesova</h4>
+          <button></button>
+        </div>
+        <p className="date">December 9, 2023</p>
+        <h3>
+          If we quantify the alarm, we can get to the <br></br> FTP pixel
+          through the online SSL interface!{" "}
+        </h3>
+        <p className="text-content">
+          Omnis perspiciatis qui quia commodi sequi modi. Nostrum quam aut
+          cupiditate est facere omnis possimus. Tenetur similique nemo illo
+          soluta molestias facere quo. Ipsam totam facilis delectus nihil quidem
+          soluta vel est omnis.
+        </p>
+        <p className="readMore">Read more...</p>
+      </section>
+      <footer>
+        <p>Modul D</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
